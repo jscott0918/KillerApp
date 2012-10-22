589b01966c3460aae8490aadd218a1d6c51588d8
Joe Scott <tscott2@g.clemson.edu>
2012-10-22 16:42:42 -0400

    source code for android app

---
+++ KillerAppRejji/src/com/killerapprejji/MainActivity.java
@@ -0,0 +1,26 @@
+package com.killerapprejji;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public boolean onClickAttackButton(View view){
+    	setContentView(R.layout.activity_attack);
+    	return true;
+    }
+}
