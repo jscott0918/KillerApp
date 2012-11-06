8bfcc20cbd8867c919a96e290216fe153ebea195
Joe Scott <tscott2@g.clemson.edu>
2012-11-05 19:52:40 -0500

    Added junit testing to commit

---
+++ JunitKillerAppRejji/src/com/killerapprejji/test/TestModel2.java
@@ -0,0 +1,19 @@
+package com.killerapprejji.test;
+
+import junit.framework.TestCase;
+
+public class TestModel2 extends TestCase {
+
+	public TestModel2(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
