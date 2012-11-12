290bcfd573eab8e2847c752f5a3677660e016d94
Joe Scott <tscott2@g.clemson.edu>
2012-11-12 16:44:51 -0500

    Fixed a warning in Interaction History class

--- KillerAppRejji/src/com/killerapprejji/InteractionHistory.java
+++ KillerAppRejji/src/com/killerapprejji/InteractionHistory.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public  class InteractionHistory implements Serializable, UpdateInteraction{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2577026923314776978L;
 	public static InteractionHistory interactionHistory = null;
 	private static ArrayList<Event> eventList;
 	
