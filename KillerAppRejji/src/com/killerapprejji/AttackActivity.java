b94092642593e4c3130840359b94e2a8b488320b
Joe Scott <tscott2@g.clemson.edu>
2012-11-27 12:20:25 -0500

    adding new changes.  Attack activity is not currently working, causes a crash

--- KillerAppRejji/src/com/killerapprejji/AttackActivity.java
+++ KillerAppRejji/src/com/killerapprejji/AttackActivity.java
@@ -2,14 +2,19 @@ package com.killerapprejji;
 //testings
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class AttackActivity extends Activity{
-	
+	ProgressBar progressBar = (ProgressBar)findViewById(R.id.attack_progress_bar);
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);    
         setContentView(R.layout.activity_attack);
-        //rest of the code
+        progressBar.setProgress(progressBar.getMax());
+        
+	}
+	private void initiateNFCAttack(){
+		
 	}
 }
 
