1598bc2b58202c2ce041f58fff6ad0476bbebdd3
Joe Scott <tscott2@g.clemson.edu>
2012-11-26 19:37:47 -0500

    Adding changes to SetInfo

--- KillerAppRejji/src/com/killerapprejji/SetInfo.java
+++ KillerAppRejji/src/com/killerapprejji/SetInfo.java
@@ -11,21 +11,29 @@ public class SetInfo extends Activity {
 
 	Button button = null;
 	EditText editTextObject = null;
+	OnClickListener onClickListener = new OnClickListener() {
+	    public void onClick(View v) {
+	    	saveButtonClick();
+	    }
+		
+	};
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    button = (Button)findViewById(R.id.save_player_info_button);
+	    button.setOnClickListener(onClickListener);
+	    editTextObject = (EditText)findViewById(R.id.display_name_edittext);
 
 	    
 	    // TODO Auto-generated method stub
+	
 	}
-/*	private OnClickListener  = new OnClickListener() {
-	    public void onClick(View v) {
-	    	InteractionHistory intHistory = InteractionHistory.getInstance();
-	    	intHistory.;
-	    }
-		
-	};*/
+	
+	private void saveButtonClick(){
+		InteractionHistory.getInstance().setDisplayName(editTextObject.getText().toString());
+		super.finish();
+	}
 
 }
