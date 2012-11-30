acb48dfb0e559c0d1059a97b31508a12b8b1d507
Joe Scott <tscott2@g.clemson.edu>
2012-11-30 17:28:49 -0500

    messed up commit

--- KillerAppRejji/src/com/killerapprejji/MainActivity.java
+++ KillerAppRejji/src/com/killerapprejji/MainActivity.java
@@ -11,36 +11,8 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //doSplashScreen();
         setContentView(R.layout.activity_main);
-        /*   setContentView(R.layout.splash);
-        
-        final boolean _active = true;
-        final int _splashTime = 5000; // time to display the splash screen in ms
-
-        
-        //GET SPLASH DISPLAYED ON TIMER
-        Thread splashThread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    int waited = 0;
-                    while(_active && (waited < _splashTime)) {
-                        sleep(100);
-                        if(_active) {
-                            waited += 100;
-                        }
-                    }
-                } catch(InterruptedException e) {
-                    // do nothing
-                } finally {
-                    finish();
-                    startActivity(new Intent("com.droidnova.android.splashscreen.MyApp"));
-                    stop();
-                }
-            }
-        };
-        splashThread.start();*/
+       
     }
     
 
