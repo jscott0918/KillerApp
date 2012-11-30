fcb26e24d395f4695220de3c123cfd15828809b7
John Butchko <jbutchko@gmail.com>
2012-11-30 17:31:43 -0500

    Fixed Splash Screen

--- KillerAppRejji/src/com/killerapprejji/MainActivity.java
+++ KillerAppRejji/src/com/killerapprejji/MainActivity.java
@@ -12,34 +12,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-        
-        final boolean _active = true;
-        final int _splashTime = 5000; // time to display the splash screen in ms
-
-        
-        //GET SPLASH DISPLAYED ON TIMER
-        Thread splashThread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    int waited = 0;
-                    while(_active && (waited < _splashTime)) {
-                        sleep(100);
-                        if(_active) {
-                            waited += 100;
-                        }
-                    }
-                } catch(InterruptedException e) {
-                    // do nothing
-                } finally {
-                    finish();
-                    startActivity(new Intent("com.droidnova.android.splashscreen.MyApp"));
-                    stop();
-                }
-            }
-        };
-        splashThread.start();
+        setContentView(R.layout.activity_main);
     }
 
     @Override
