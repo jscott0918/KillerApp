14ffa5a718cb55a6f589ae814e38c1c914351caf
itaylor <ian.m.taylor1@gmail.com>
2012-12-02 14:49:40 -0500

    Added recordAttack() method to SqlDatabaseHelper
    Also changed create command to not overwrite existing table

--- KillerAppRejji/src/com/killerapprejji/SqlDatabaseHelper.java
+++ KillerAppRejji/src/com/killerapprejji/SqlDatabaseHelper.java
@@ -6,11 +6,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class SqlDatabaseHelper extends SQLiteOpenHelper {
 
-	private static final int DATABASE_VERSION = 2;
+	private static final int DATABASE_VERSION = 1;
     private static final String DICTIONARY_TABLE_NAME = "dictionary";
     private static final String DATABASE_NAME = "KillerAppDB";
     private static final String DICTIONARY_TABLE_CREATE =
-                "CREATE TABLE " + DICTIONARY_TABLE_NAME 
+                "CREATE TABLE IF NOT EXISTS" + DICTIONARY_TABLE_NAME 
                 + " (" +
                 "attacker" + " TEXT, " +
                 "attackerid" + " VARCHAR(64)" +
@@ -26,12 +26,28 @@ public class SqlDatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(DICTIONARY_TABLE_CREATE);
+		
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void recordAttack(Event attack)
+	{
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.execSQL(
+				"INSERT INTO " + DICTIONARY_TABLE_NAME 
+				+ " (attacker, attackerid, defender, defenderid) "
+				+ " VALUES ( " 
+					+ attack.getAttacker() + ", "
+					+ attack.getAttackerId() + ", "
+					+ attack.getDefender() + ", "
+					+ attack.getDefenderId() + ", "
+				+ " ) "
+				);
 	}
 
 }
