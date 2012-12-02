d376b9fc65af25bcacff794e1c18cc352e021861
Joe Scott <tscott2@g.clemson.edu>
2012-12-02 15:59:06 -0500

    bogus commit

--- KillerAppRejji/src/com/killerapprejji/InteractionHistory.java
+++ KillerAppRejji/src/com/killerapprejji/InteractionHistory.java
@@ -26,6 +26,7 @@ public class InteractionHistory implements Serializable, UpdateInteraction{
 	/*public NfcAdapter getNFC(){
 		return nfc;
 	}*/
+	//test for simple commit
 	
 	private InteractionHistory(){
 		eventList = new ArrayList<Event>();
