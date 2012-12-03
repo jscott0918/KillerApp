23c1edd6736147fa7f5bcb5421bd6dedcc841134
Joe Scott <tscott2@g.clemson.edu>
2012-12-02 21:57:43 -0500

    reverting android manifest and adding changes to main activity

--- KillerAppRejji/src/com/killerapprejji/MainActivity.java
+++ KillerAppRejji/src/com/killerapprejji/MainActivity.java
@@ -5,12 +5,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	Button attackButton = null;
+	Button defendButton = null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        attackButton = (Button)findViewById(R.id.attack_button);
+        defendButton = (Button)findViewById(R.id.defend_button);
         setContentView(R.layout.activity_main);
         
     }
@@ -29,6 +35,26 @@ public class MainActivity extends Activity {
     public boolean onClickMenuButton(View view){
     	Intent startNewActivityOpen = new Intent(this, SetInfo.class);
     	startActivityForResult(startNewActivityOpen, 0);
+    	return true;
+    }
+    
+    public boolean disableDefendButton(){
+    	defendButton.setClickable(false);
+    	return true;
+    }
+    
+    public boolean disableAttackButton(){
+    	attackButton.setClickable(false);
+    	return true;
+    }
+    
+    public boolean enableDefendButton(){
+    	defendButton.setClickable(true);
+    	return true;
+    }
+    
+    public boolean enableAttackButton(){
+    	attackButton.setClickable(true);
     	return true;
     }
     
