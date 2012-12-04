7e84610be8e3ddeb6ccb996d6fda8a08580519ce
Joe Scott <tscott2@g.clemson.edu>
2012-12-03 20:15:53 -0500

    Refactored the NFC factory class into MainActivity to fix issues with activity not stopping/starting correctly.

--- KillerAppRejji/src/com/killerapprejji/MainActivity.java
+++ KillerAppRejji/src/com/killerapprejji/MainActivity.java
@@ -6,10 +6,17 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.app.Activity;
-import android.content.ContextWrapper;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentFilter.MalformedMimeTypeException;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -19,11 +26,35 @@ public class MainActivity extends Activity {
 	
 	Button attackButton = null;
 	Button defendButton = null;
+	private static NfcAdapter mNfcAdapter = null;
+	private static PendingIntent mNfcPendingIntent = null;
+	private static IntentFilter mNdefExchangeFilters[] = null;
 	public static final String EXTRA_MESSAGE = "com.killerappRejji.MainActivity.MESSAGE";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		mNfcPendingIntent = PendingIntent.getActivity(this, 0,
+			    new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+		// Intent filters for exchanging over p2p.
+		IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+		IntentFilter defendDetected = new IntentFilter("defend");
+		Intent getIntent = getIntent();
+		String intMessage = getIntent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		Log.d("OnCreate()", "intMessage: " + intMessage);
+		if(intMessage.equals("defend")){
+			setDefendMessage();
+		} else if (intMessage.equals("attack")){
+			setAttackMessage();
+		}
+		try {
+		    ndefDetected.addDataType("application/com.killerapprejji.NfcHandle");
+		} catch (MalformedMimeTypeException e) {
+			Log.e("MalformedMimTypeException", e.getStackTrace().toString());
+		}
+		Log.d(this.toString(), "mNfcPendingIntent: ");
+		mNdefExchangeFilters = new IntentFilter[] { ndefDetected,defendDetected };
         //NfcHandle nfc = new NfcHandle();
         //nfc.setIdleMessage();
         attackButton = (Button)findViewById(R.id.attack_button);
@@ -31,6 +62,114 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
     }
+    
+    public void setAttackMessage(){
+		InteractionHistory intHist = InteractionHistory.getInstance();
+		NdefMessage attackNdefMessage = null;
+		NdefRecord[] ndefRecords = new NdefRecord[10];
+		ndefRecords[0] = NdefRecord.createMime("application/com.killerapprejji.NfcHandle", new String("attack,attacker:"
+				+ intHist.getDisplayName() 
+				+ ",attackerid:" + "1").getBytes());
+		attackNdefMessage = new NdefMessage(ndefRecords[0]);
+		mNfcAdapter.setNdefPushMessage(attackNdefMessage, this);
+	}
+	
+	public void setDefendMessage(){
+		InteractionHistory intHist = InteractionHistory.getInstance();
+		Log.d("setDefendMessage()", "In setDefendMessage");
+		NdefMessage attackNdefMessage = null;
+		NdefRecord[] ndefRecords = new NdefRecord[10];
+		ndefRecords[0] = NdefRecord.createMime("application/com.killerapprejji.NfcHandle", new String("defend,defender:"
+				+ intHist.getDisplayName() 
+				+ ",defenderid:" + "1").getBytes());
+		attackNdefMessage = new NdefMessage(ndefRecords[0]);
+		
+		// need to come up with a way to end if the above try/catch fails
+		mNfcAdapter.setNdefPushMessage(attackNdefMessage, this);
+	}
+	public void setIdleMessage(){
+		InteractionHistory intHist = InteractionHistory.getInstance();
+		NdefMessage attackNdefMessage = null;
+		NdefRecord[] ndefRecords = new NdefRecord[10];
+		ndefRecords[0] = NdefRecord.createMime("application/com.killerapprejji.NfcHandle", new String("attack,attacker:"
+				+ intHist.getDisplayName() 
+				+ ",attackerid:" + "1").getBytes());
+		attackNdefMessage = new NdefMessage(ndefRecords[0]);
+		// need to come up with a way to end if the above try/catch fails
+		mNfcAdapter.setNdefPushMessage(attackNdefMessage, this);
+	}
+	public void setDeadMessage(){
+		InteractionHistory intHist = InteractionHistory.getInstance();
+		NdefMessage attackNdefMessage = null;
+		NdefRecord[] ndefRecords = new NdefRecord[10];
+		ndefRecords[0] = NdefRecord.createMime("application/com.killerapprejji.NfcHandle", new String("attack,attacker:"
+				+ intHist.getDisplayName() 
+				+ ",attackerid:" + "1").getBytes());
+		attackNdefMessage = new NdefMessage(ndefRecords[0]);
+		// need to come up with a way to end if the above try/catch fails
+		mNfcAdapter.setNdefPushMessage(attackNdefMessage, this);
+	}
+	
+	protected void onNewIntent(Intent intent) {
+	    // NDEF exchange mode
+	    if (NfcAdapter.ACTION_NDEF_DISCOVERED == (intent.getAction())) {
+	        NdefMessage[] msgs = getNdefMessages(intent);
+	        for(int i = 0; i < msgs.length; i++){
+	        	if(new String(msgs[0].toByteArray()).contains("attack")){
+	        		
+	        	}
+	        }
+	        Toast.makeText(this, "You got 'em!", Toast.LENGTH_LONG).show();
+	    }
+	    else if("defend" == intent.getAction()){
+	    	Log.d("found defend intent", "toast message may pop up");
+	    	Toast.makeText(this, "Defend action intent filtered by onNewIntent in NfcHandle", Toast.LENGTH_LONG).show();
+	    }
+	    
+	    finish();
+	}
+
+	private void enableNdefExchangeMode() {
+		try {
+			mNfcAdapter.setNdefPushMessage(new NdefMessage(new String().getBytes()),this);
+		} catch (FormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent, 
+	        mNdefExchangeFilters, null);
+	}
+	
+	NdefMessage[] getNdefMessages(Intent intent) {
+	    // Parse the intent
+	    NdefMessage[] msgs = null;
+	    String action = intent.getAction();
+	    if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+	        || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+	        Parcelable[] rawMsgs = 
+	            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+	        if (rawMsgs != null) {
+	            msgs = new NdefMessage[rawMsgs.length];
+	            for (int i = 0; i < rawMsgs.length; i++) {
+	                msgs[i] = (NdefMessage) rawMsgs[i];
+	            }
+	        } else {
+	            // Unknown tag type
+	            byte[] empty = new byte[] {};
+	            NdefRecord record = 
+	                new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+	            NdefMessage msg = new NdefMessage(new NdefRecord[] {
+	                record
+	            });
+	            msgs = new NdefMessage[] {
+	                msg
+	            };
+	        }
+	    } else {
+	        finish();
+	    }
+	    return msgs;
+	}
     
     public void onClickViewHistory(View view){
     	Intent startNewActivityOpen = new Intent(this, DisplayInteractions.class);
@@ -70,10 +209,7 @@ public class MainActivity extends Activity {
     	boolean ret = false;
     	if(ActionAvailability.getInstance().getCanAttack() < Calendar.getInstance().getTimeInMillis()){
     		Log.d("MainActivity", "starting onClickAttackButton");
-        	Intent setAttackMessage = new Intent("attack", null, this, NfcHandle.class);
-        	setAttackMessage.putExtra(EXTRA_MESSAGE, "attack");
-        	startActivity(setAttackMessage);
-        	
+        	setAttackMessage();
         	Intent startNewActivityOpen = new Intent(this, AttackActivity.class);
     		startActivityForResult(startNewActivityOpen, 0);
     		
@@ -89,9 +225,7 @@ public class MainActivity extends Activity {
     
     public boolean onClickDefendButton(View view){
     	Log.d("MainActivity", "starting onClickDefendButton");
-    	Intent setDefendMessage = new Intent("defend", null, this, NfcHandle.class);
-    	setDefendMessage.putExtra(EXTRA_MESSAGE, "defend");
-    	startActivity(setDefendMessage);
+    	setDefendMessage();
     	Intent startNewActivityOpen = new Intent(this, DefendActivity.class);
     	startActivityForResult(startNewActivityOpen, 0);
     	return true;
